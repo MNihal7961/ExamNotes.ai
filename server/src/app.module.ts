c98b8805b42490c59b656c90ea7bc43b9a56190e
MNihal7961 <mnihal7961@gmail.com>
2026-09-01 16:28:49 +0530

    feat(server): update database configuration and modify welcome message in AppService

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,16 +3,19 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { databaseConfigFactory } from './config/database.config';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        uri: config.get<string>('MONGODB_URI'),
-      }),
+      useFactory: databaseConfigFactory,
     }),
+    UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
